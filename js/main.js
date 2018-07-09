d2ec063872777c09289590e16d0bb8fce18f33da
Tom Endacott <tendacott@spartaglobal.com>
2018-07-09 12:16:50 +0100

    Positioned and styled HTML elements

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-$(function(){
-console.log("Hello world");
+$(function() {
+  console.log("Hello world!");
 
-
-
-
-
+  var timeleft = 30;
+  var downloadTimer = setInterval(function(){
+  timeleft--;
+  document.getElementsByClass("time").textContent = "Time: " + timeleft;
+  if(timeleft <= 0)
+      clearInterval(downloadTimer);
+  },1000);
 
 
 
