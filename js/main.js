252670d49f369ffe06b7edb8ca2c01aa5889dd92
Tom Endacott <tendacott@spartaglobal.com>
2018-07-09 17:31:09 +0100

    Images appear at random on refresh and start time is delayed

--- js/main.js
+++ js/main.js
@@ -1,49 +1,55 @@
 $(function() {
-  console.log("Hello world!");
 
-  var imgRaptor = "../images/raptorfull-sm-l.png"
-  var imgJeff = "../images/jeffgoldblum-sm.png"
+  console.log("Hello Jurassic World!");
+
+  setTimeout(function(){
+    showChar();
+  }, 5000);
 
   // Countdown Timer
-  var timeLeft = 30;
-  var timerId = setInterval(countdown, 1000);
+  setTimeout(function() {
+    var timeLeft = 30;
+    var timerTime = setInterval(countdown, 1000);
 
-  function countdown() {
-    if (timeLeft == 0) {
-      $(".time").html("TIME'S UP!")
-    } else {
-      $(".timeCounter").html(timeLeft);
-      timeLeft--;
-    }
-  };
+    function countdown() {
+      if (timeLeft == 0) {
+        $(".time").html("TIME'S UP!")
+      } else {
+        $(".timeCounter").html(timeLeft);
+        timeLeft--;
+        console.log(timeLeft);
+      }
+    };
+  }, 4000);
+
+
 
   // Random images to appear
-  var imageArray = ["dino1", "dino1","dino1", "human1"];
+  var imageArray = ["dino1"];
 
-  function generateRandomForArray() {
-      var num = Math.floor(Math.random() * 4);
+  function generateRandomImage() {
+      var num = Math.floor(Math.random() * 1);
       return num;
   }
 
   function generateRandomLeft() {
-      var num = Math.floor(Math.random() * 1336);
+      var num = Math.floor(Math.random() * 1250);
       return num;
   }
 
   function generateRandomTop() {
-      var num = Math.floor(Math.random() * 675);
+      var num = Math.floor(Math.random() * 640);
       return num;
   }
 
   function showChar() {
-      var char = imageArray[generateRandomForArray()];
+      var char = imageArray[generateRandomImage()];
       $(".random-image").append("<img src='images/" + char + ".png'>");
       var left = generateRandomLeft();
       var top = generateRandomTop();
       $(".random-image").last().css({"position":"absolute","top": top + "px", "left": left + "px"});
   }
 
-  showChar();
 
 
 
@@ -54,4 +60,9 @@ $(function() {
 
 
 
-});
+
+
+
+
+
+}); //Close for document load
