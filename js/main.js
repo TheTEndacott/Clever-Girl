ef0cb20a549da42d4aa68d5d757beea516a481f1
Tom Endacott <tendacott@spartaglobal.com>
2018-07-10 12:32:16 +0100

    When timer reaches the end the enemies are no longer displayed

--- js/main.js
+++ js/main.js
@@ -3,67 +3,77 @@ $(function() {
   console.log("Hello Jurassic World!");
 
   var playerScore = 0;
+  var timeLeft = 6; // -------------------------------------------------------- Set to 30 -------------------------------------------------
 
-  // Countdown Timer
+  // Countdown Timer ----------
   setTimeout(function() {
-    var timeLeft = 30;
     setInterval(countdown, 1000);
+  }, 1000);
 
-    function countdown() {
-      if (timeLeft == 0) {
-        $(".time").html("TIME'S UP!")
-      } else {
-        $(".timeCounter").html(timeLeft);
-        timeLeft--;
-        console.log(timeLeft);
-      }
-    };
-  }, 2000);
+  function countdown() {
+    if (timeLeft == 0) {
+      $(".time").html("TIME'S UP!");
+      clearInterval(showChar());
+      clearInterval(intervalChar);
+      clearInterval(stopChar());
+      
+    } else {
+      $(".timeCounter").html(timeLeft);
+      timeLeft--;
+      console.log(timeLeft);
+    }
+  };
 
+  // Stop game running when timer runs out ----------
+  function stopChar() {
+    $(".random-image img:last-child").remove(); // Can be replace stopchar in enemyRemove
+  }
 
-  // Random images to appear
+  // Random images to appear ----------
   var imageArray = ["dino1"];
 
-  function generateRandomImage() {
+  function randomImage() {
       var num = Math.floor(Math.random() * 1);
       return num;
   }
 
-  function generateRandomLeft() {
+  function randomLeft() {
       var num = Math.floor(Math.random() * 1250);
       return num;
   }
 
-  function generateRandomTop() {
+  function randomTop() {
       var num = Math.floor(Math.random() * 640);
       return num;
   }
 
   function showChar() {
-      var char = imageArray[generateRandomImage()];
+      var char = imageArray[randomImage()];
       $(".random-image").append("<img src='images/" + char + ".png'>");
-      var left = generateRandomLeft();
-      var top = generateRandomTop();
+      var left = randomLeft();
+      var top = randomTop();
       $(".random-image").last().css({"position":"absolute","top": top + "px", "left": left + "px"});
       console.log("Raptor");
 
-      setTimeout(function(){
-        $(".random-image img:last-child").remove();
-      }, 1000); // Enemy cleared after 1s
+      var enemyRemove = setTimeout(function(){
+        stopChar();
+      }, 1000); // Enemy removed after 1s
   }
 
+  // Delay function for showChar ----------
+  var intervalChar = setInterval(showChar, 2000); // Enemy displayed for 2s but cleared by timeout after 1s
 
-  // Delay function for showChar
-  setInterval(function(){
-    showChar();
-  }, 2000); // Enemy displayed for 2s but cleared by timeout after 1s
-
-  // Click event for
+  // Click event for enemy and update score counter ----------
   $(".random-image").click(function(){
     newScore = playerScore++;
     $(".scoreCounter").html(newScore);
     console.log("SCORE");
   });
+
+
+
+
+
 
 
 
